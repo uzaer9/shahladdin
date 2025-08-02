6b33abc927d6def57628d798b7d4dd24b3a76e90
uzaer9 <108221436+uzaer9@users.noreply.github.com>
2025-08-03 03:23:53 +0530

    Setup GitHub Pages deployment for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "./",      // ✅ This is critical
-  plugins: [react()]
+  base: "/shahladdin/",  // 💡 Use repo name here
+  plugins: [react()],
 })
